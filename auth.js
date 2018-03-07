a000b12e55c01a1c512b3208c8857cf076ac37e7
YASHWANT KUMAR SONKAR <31109246+Sonkaryasshu@users.noreply.github.com>
2018-03-07 17:16:27 +0530

    diffrent signout function

--- auth.js
+++ auth.js
@@ -1,13 +1,5 @@
 var btn = document.getElementById('btn');
-btn.addEventListener('click', 	function(){
-  console.log('SignOut button clicked');
-  var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User SignedOut');
-      document.getElementById('log').style.display="block";
-      document.getElementById('data').style.display="none";
-    });
-});
+btn.addEventListener('click', signOut());
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   document.getElementById('log').style.display="none";
@@ -16,3 +8,12 @@ function onSignIn(googleUser) {
   document.getElementById('pic').src= profile.getImageUrl();
   document.getElementById('email').innerHTML="Email: " + profile.getEmail(); // This is null if the 'email' scope is not present.
 }
+function signOut() {
+    console.log('SignOut button clicked');
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+      document.getElementById('log').style.display="block";
+      document.getElementById('data').style.display="none";
+    });
+  }
