c292fc3344e8cb034e8c86d847855c62e97a7ad0
YASHWANT KUMAR SONKAR <31109246+Sonkaryasshu@users.noreply.github.com>
2018-03-07 16:40:01 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,13 @@
-
+var btn = document.getElementById('btn');
+btn.addEventListener('click', 	function(){
+  console.log('SignOut button clicked');
+  var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User SignedOut');
+      document.getElementById('log').style.display="block";
+      document.getElementById('data').style.display="none";
+    });
+});
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   document.getElementById('log').style.display="none";
@@ -6,11 +15,4 @@ function onSignIn(googleUser) {
   document.getElementById('username').innerHTML= profile.getName();
   document.getElementById('pic').src= profile.getImageUrl();
   document.getElementById('email').innerHTML="Email: " + profile.getEmail(); // This is null if the 'email' scope is not present.
-}
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      document.getElementById('log').style.display="block";
-      document.getElementById('data').style.display="none";
-    });
 }
